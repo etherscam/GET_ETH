36f65c88ce3ea2eac62c2a24a676aa925caf0423
etherscam <40215726+etherscam@users.noreply.github.com>
2021-01-20 09:12:07 +0300

    Update GET.py

--- GET.py
+++ GET.py
@@ -1,3 +1,4 @@
+
 import configparser 
 import requests
 import os
@@ -20,35 +21,42 @@ try:
     my_wallet = config['SENDER']['MY_WALLET']#Кошелек для получения эфира#Приватный
     gas_price = int(config['SENDER']['GAS_PRICE'])
     try: 
-         with open ("wallets") as f:
+        with open ("wallets") as f:
             wallets = [line.rstrip() for line in f]    
         with open ("private_keys") as p:
             private_keys = [line.rstrip() for line in p]
             if wallets and private_keys and len(wallets)==len(private_keys):
-                summ_eth  = am_eth*len(wallets)
-                if click.confirm('Вы хотите отправить %sETH на %s'%(summ_eth,my_wallet), default=True):
+
+                if click.confirm('Вы хотите отправить ETH на %s'%(my_wallet), default=True):
+                    wallets_2 = []
                     for i in wallets:
                         d = w3.toChecksumAddress(i)
                         wallets_2.append(d)
-                    #am = w3.toWei(am_eth, 'ether')
-                    nonce = w3.eth.getTransactionCount(w3.toChecksumAddress(wallet))
+
                     for i,k in zip(wallets_2,private_keys):
                         try:
+                            am  = w3.eth.getBalance(i) - (w3.toWei(gas_price,'gwei')*21000)
+                            print(am)
+                            nonce = w3.eth.getTransactionCount(w3.toChecksumAddress(i))
                             txn =dict({
                                  'gas': 21000,
                                  'gasPrice': w3.toWei(gas_price,'GWEI'),
                                  'to':my_wallet,
-                                 'value': w3.eth.getBalance(i) - w3.fromWei((w3.toWei(gas_price,'gwei')*21000)),
+                                 'value': am,
                                  'nonce': nonce,
 
                             })
                             signed_txn = w3.eth.account.sign_transaction(txn, private_key=k)
                             tx_hash = w3.eth.sendRawTransaction(signed_txn.rawTransaction)
                             print(tx_hash.hex()) 
-                        except:
+                        except Exception as ex:
+                            print(ex)
                             print('OOPS Check:', i)
                     print('Выполнено, Ожидайте...')
     
     except Exception as ex:
         print(ex)
         print('Проверьте наличие файлов wallets и private_keys')
+
+except Exception as ex:
+        print(ex)
